79e7d582a58b202c03ada45b3b6a56257b210869
overdiv <stancode203@gmail.com>
2020-10-08 01:52:44 +0900

    #6 로그인 에러처리

--- app-note/src/components/LoginForm.vue
+++ app-note/src/components/LoginForm.vue
@@ -32,14 +32,18 @@ export default {
   },
   methods: {
     async submitForm() {
-      const userData = {
-        username: this.username,
-        password: this.password,
-      };
-      const { data } = await loginUser(userData);
-      console.log(data.user.username);
-      this.logMessage = `${data.user.username}님 환영합니다`;
-      this.initForm();
+      try {
+        const userData = {
+          username: this.username,
+          password: this.password,
+        };
+        const { data } = await loginUser(userData);
+        this.logMessage = `${data.user.username}님 환영합니다`;
+      } catch (error) {
+        this.logMessage = error.response.data;
+      } finally {
+        this.initForm();
+      }
     },
     initForm() {
       this.username = '';
