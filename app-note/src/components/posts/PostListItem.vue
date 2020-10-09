0b4db95ea06c3b2f9e8684315df7a0cb364e9a05
overdiv <stancode203@gmail.com>
2020-10-10 03:15:04 +0900

    메인페이지 생성, 데이터 리스트 노출 적용

---
+++ app-note/src/components/posts/PostListItem.vue
@@ -0,0 +1,50 @@
+<template>
+  <li>
+    <strong>
+      {{ propsItem.title }}
+    </strong>
+    <p>
+      {{ propsItem.contents }}
+    </p>
+    <span>
+      {{ propsItem.createdAt }}
+    </span>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    propsItem: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import '../../assets/scss/utils/var_color';
+li {
+  margin-top: 10px;
+  padding: 13px 0;
+  border-bottom: 1px solid $border-gray;
+
+  &:first-child {
+    border-top: 1px solid $border-gray;
+  }
+}
+strong {
+  display: block;
+}
+p {
+  padding: 15px 0 30px;
+  line-height: 19px;
+}
+span {
+  display: block;
+  font-size: 12px;
+  color: #777;
+  text-align: right;
+}
+</style>
--- app-note/src/views/MainPage.vue
+++ app-note/src/views/MainPage.vue
@@ -1,11 +1,37 @@
 <template>
   <div>
-    main
+    <h2>Note List</h2>
+    <ul>
+      <PostListItem
+        v-for="postItem in postItems"
+        :key="postItem._id"
+        :propsItem="postItem"
+      />
+    </ul>
   </div>
 </template>
 
 <script>
-export default {};
+import PostListItem from '@/components/posts/PostListItem.vue';
+import { fetchPosts } from '@/api';
+export default {
+  components: {
+    PostListItem,
+  },
+  data() {
+    return {
+      postItems: [],
+    };
+  },
+  methods: {
+    async fetchData() {
+      const { data } = await fetchPosts();
+      console.log(data);
+      this.postItems = data.posts;
+    },
+  },
+  created() {
+    this.fetchData();
+  },
+};
 </script>
-
-<style></style>
